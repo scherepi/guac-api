62331eec91661e1814094054bf5dab03d5c4dc6e
scherepi <scherejoaquin@gmail.com>
2025-03-04 21:52:18 -0600

    Testing static file serving

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const dataPath = "./data/";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.use(express.static("data"));
+
+app.get("/api/hello", (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/api/wilder", (req, res) => {
+    res.send()
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
